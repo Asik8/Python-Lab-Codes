7f12c88c108c7733fa6c4b133508ffae2e385561
Asik8 <motasembillah84@gmail.com>
2024-04-04 17:30:41 +0600

    Code Added

---
+++ Prctice1(Apna College).py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      acer
+#
+# Created:     26/06/2023
+# Copyright:   (c) acer 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+firstName= "Tony"
+LastName= "Stark"
+age= 51
+p= "is a genious"
+
+print(firstName)
+print(LastName)
+##print(age "years Old)
+print("")
+
