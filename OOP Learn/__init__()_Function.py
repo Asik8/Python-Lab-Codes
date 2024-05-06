75c52de894557b20b32714ca9b0adfa17dd02686
Asik8 <motasembillah84@gmail.com>
2024-05-06 21:13:32 +0600

    code updated

--- OOP Learn/__init__()_Function.py
+++ OOP Learn/__init__()_Function.py
@@ -1,11 +1,11 @@
-class person:
-    def __init__(self,name,number,age,gender):
+class person: # Class
+    def __init__(self,name,number,age,gender): # Constructor
         self.name = name
         self.number = number
         self.age = age
         self.gender = gender
 
-p1 = person("Asik",115646515,23,"male")
-p2 = person("Akib",115646516,13,"male")
+p1 = person("Asik",115646515,23,"male") # Object
+p2 = person("Akib",115646516,13,"male") # Object
 print(p1.name,p1.number,p1.age,p1.gender)
 print(p2.name,p2.number,p2.age,p2.gender)
