d528bb12afcbf143654c2b534b61b9ff83768a2e
Asik8 <motasembillah84@gmail.com>
2024-05-28 22:28:30 +0600

    numpy shape

---
+++ OOP Learn/Numpy/8_ArrayShape.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+# print(arr.shape) # shape represents the number of rows and the number of columns
+
+a = np.array([1, 2, 3, 4], ndmin=5)
+# print(a)
+# print('shape of array :', a.shape) # shape off 5d array
+
